64e3b6d3a18ecfe7e6372c7731e2f88682a86dd3
Joao Holanda <joaocmdt505@gmail.com>
2023-09-04 21:00:38 -0300

    lista de adjacências

---
+++ graph-theory/lista-adjacencias.py
@@ -0,0 +1,26 @@
+class Grafo:
+    
+  def __init__(self, vertices):
+    self.vertices = vertices 
+    self.grafo = [[] for i in range(self.vertices)]
+
+  def adiciona_aresta(self, u, v):
+    #pensando em grafo direcionado
+    self.grafo[u-1].append(v)
+    #self.grafo[v-1].append(u) se o grafo não for direcionado sem peso nas arestas
+    #caso seja um grafo valorado, adicionar parâmetro peso na função e no .append([v,peso])
+
+  def mostra_lista(self):
+    for i in range(self.vertices):
+      print(f'{i+1}:', end='  ')
+      for j in self.grafo[i]:
+        print(f'{j}  ->', end='  ')
+      print('')
+
+g = Grafo(4)
+g.adiciona_aresta(1,2)
+g.adiciona_aresta(1,3)
+g.adiciona_aresta(1,4)
+g.adiciona_aresta(2,3)
+
+g.mostra_lista()
